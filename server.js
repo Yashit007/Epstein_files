7fffd221f8b33c5c5ce1b929c15b13a31228d8b8
Yashit007 <150842214+Yashit007@users.noreply.github.com>
2026-01-16 14:23:50 +0100

    Trial for github pages

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 
 // Route for home page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'Voter_Sathi_Final.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.get('')
 // Start server
